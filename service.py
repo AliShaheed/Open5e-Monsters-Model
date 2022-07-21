2044250f5884b02f35e5600a190b0b9229143f59
AliShaheed <alishaheed1138@gmail.com>
2022-07-21 22:35:41 +0100

    Revised comments

--- service.py
+++ service.py
@@ -2,8 +2,8 @@ from monster_stats import Stats
 
 monster = Stats('adult-black-dragon') # pass monster name (needs to be hyphenated) into model
 
-monster_basic = monster.get_stats('basic') # store stats as attributes of monster_status object
-monster_abilities =  monster.get_stats('ability')
+monster_basic = monster.get_stats('basic') # store stats as attributes of monster_basic object
+monster_abilities =  monster.get_stats('ability') # store ability stats as attributes of monster_abilities object
 
 print(monster_basic.name) # print name attribute, returns 'Adult Black Dragon'
 print(monster_abilities.special_abilities) # print special_abilities attribute, returns list of dictionaries
