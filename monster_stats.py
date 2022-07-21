436c464c4053415425a2250f74674956558dd6c4
AliShaheed <alishaheed1138@gmail.com>
2022-07-21 17:55:46 +0100

    added comments

--- monster_stats.py
+++ monster_stats.py
@@ -2,10 +2,10 @@ from monster_endpoint_model import BasicsModel
 import requests
 
 class BasicStats():
-    def __init__(self, monster) -> None:
+    def __init__(self, monster) -> None:  # initialisation monster data from open5e
         self.url = f'https://api.open5e.com/monsters/{monster}'
         self.response = requests.get(self.url)
-        self.monster_data = self.response.json()
+        self.monster_data = self.response.json() # format to json
 
     def get_stats(self):
-        return BasicsModel(self.monster_data)
+        return BasicsModel(self.monster_data) # return modelled stats
