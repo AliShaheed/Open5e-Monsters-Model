b5bad8405cc84162f404338df386369f4c0caa98
AliShaheed <alishaheed1138@gmail.com>
2022-07-21 21:54:39 +0100

    Added abilities for output

--- service.py
+++ service.py
@@ -1,7 +1,9 @@
-from monster_stats import BasicStats
+from monster_stats import Stats
 
-monster = BasicStats('adult-black-dragon') # pass monster name (needs to be hyphenated) into model
+monster = Stats('adult-black-dragon') # pass monster name (needs to be hyphenated) into model
 
-monster_stats = monster.get_stats() # store stats as attributes of monster_status object
+monster_basic = monster.get_stats('basic') # store stats as attributes of monster_status object
+monster_abilities =  monster.get_stats('ability')
 
-print(monster_stats.name) # print name attribute, returns 'Adult Black Dragon'
+print(monster_basic.name) # print name attribute, returns 'Adult Black Dragon'
+print(monster_abilities.special_abilities) # print special_abilities attribute, returns list of dictionaries
