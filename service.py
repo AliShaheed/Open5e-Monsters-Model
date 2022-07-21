6963d30e97f904608584b0c5dedee4af7b5f43d1
AliShaheed <alishaheed1138@gmail.com>
2022-07-21 17:50:14 +0100

    added code to print monster name attribute

---
+++ service.py
@@ -0,0 +1,7 @@
+from monster_stats import BasicStats
+
+monster = BasicStats('adult-black-dragon') # pass monster name (needs to be hyphenated) into model
+
+monster_stats = monster.get_stats() # store stats as attributes of monster_status object
+
+print(monster_stats.name) # print name attribute
