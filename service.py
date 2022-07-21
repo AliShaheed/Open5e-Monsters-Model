056c19867ce196d3516bc09562c0ccdf759510ae
AliShaheed <alishaheed1138@gmail.com>
2022-07-21 18:35:42 +0100

    Revised comment

--- service.py
+++ service.py
@@ -4,4 +4,4 @@ monster = BasicStats('adult-black-dragon') # pass monster name (needs to be hyph
 
 monster_stats = monster.get_stats() # store stats as attributes of monster_status object
 
-print(monster_stats.name) # print name attribute
+print(monster_stats.name) # print name attribute, returns 'Adult Black Dragon'
